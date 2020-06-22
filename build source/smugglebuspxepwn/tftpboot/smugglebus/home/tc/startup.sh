15758291e9ab852e87a53be49c6f9362986ef1ad
Piotr Marszalik <addenial@users.noreply.github.com>
2020-06-22 14:51:01 -0400

    Update startup.sh

--- build source/smugglebuspxepwn/tftpboot/smugglebus/home/tc/startup.sh
+++ build source/smugglebuspxepwn/tftpboot/smugglebus/home/tc/startup.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+#this gets started with sudo from .profile. 
+#no need to call sudo again when running python
+
 #echo "Running SmuggleBus"
 #sudo python /home/tc/smugglebus.py
 
-echo "Running HASHsnatcher"
-sudo python3.4 /home/tc/hashsnatcher.py
+echo "Running HaShSnAtChEr"
+python3.4 /home/tc/hashsnatcher.py
 
 
 #echo "Done. Powering off"
