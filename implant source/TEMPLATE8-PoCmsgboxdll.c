4a70944283a796d4b7b296c09d8426313b9a4706
Piotr Marszalik <addenial@users.noreply.github.com>
2020-07-07 13:02:18 -0400

    Update TEMPLATE8-PoCmsgboxdll.c

--- implant source/TEMPLATE8-PoCmsgboxdll.c
+++ implant source/TEMPLATE8-PoCmsgboxdll.c
@@ -1,12 +1,18 @@
 #include <windows.h>
 
+#if BUILDING_DLL
+#define DLLIMPORT __declspec(dllexport)
+#else
+#define DLLIMPORT __declspec(dllimport)
+#endif
+
 //Compile with
 //i686-w64-mingw32-gcc -shared -o PoCmsgbox.dll TEMPLATE8-PoCmsgboxdll.c
 //
 //Code will execute when the DLL is attached 
 //On target system execute with
-//rundll32.exe Pocmsgbox.dll,any
-
+//rundll32.exe PoCmsgbox.dll,any
+//rundll32.exe PoCmsgbox.dll,EntryPoint
 
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
 {
@@ -37,8 +43,12 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
 int codehere(){
 
 	MessageBox(0, "Hello World from DLL !!\n", "Dll Injection PoC", MB_ICONINFORMATION);
-	//
-	//
 	//WinExec("cmd.exe /k C:\\windows\\system32\\cmd.exe", 0);
 	//
+}
+
+//export EntryPoint
+int EntryPoint(){
+	MessageBox(0, "EntryPoint function found!!! :) \n", "EntryPoint", MB_ICONINFORMATION);
+
 }
